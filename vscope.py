3d08f8f80c2deb56b127b64c21e5b9031be981dc
Harrison Bond <harrison@juiceanalytics.com>
2015-11-29 20:15:22 -0500

    added more robust Image attribute handling, added ability grab all attribute instances of a single attribute across all Images, and then do a freq count on the attribute's values

--- vscope.py
+++ vscope.py
@@ -1,10 +1,11 @@
 import requests as rq
 from requests.status_codes import codes
 from bs4 import BeautifulSoup as bs
-from tqdm import *
+from tqdm import tqdm
 
 import re
 import json
+from collections import defaultdict, OrderedDict
 from os import path as osp
 import os
 
@@ -12,19 +13,46 @@ from pprint import pprint as pp
 
 
 class Image:
-    parameters = (
-        'upload_date', 'is_featured', 'height', 'width',
-        'description', 'tags', 'preset', 'permalink',
-        'responsive_url', 'image_meta', '_id', 'is_video',
-        'grid_name', 'perma_subdomain', 'site_id'
+    top_level_attributes = (
+        'upload_date',
+        'is_featured',
+        'height',
+        'width',
+        'description',
+        'tags',
+        'preset',
+        'permalink',
+        'responsive_url',
+        'image_meta',
+        '_id',
+        'is_video',
+        'grid_name',
+        'perma_subdomain',
+        'site_id'
     )
+    supplementary_attributes_to_flatten = {
+        'iso': ('image_meta', 'ios'),
+        'phone_model': ('image_meta', 'model'),
+        'phone_make': ('image_meta', 'make'),
+        'preset': ('preset', 'key'),
+        'preset_bg_color': ('preset', 'color')
+    }
 
-    def __init__(self, details, session, cached_image_width=None, auto_download_file=True):
-        self.full_details = details
-        self.details = {}
-        for p in self.parameters:
-            self.__dict__[p] = details.get(p, None)
-            self.details[p] = details.get(p, None)
+    def __init__(self,
+                 details,
+                 session,
+                 cached_image_width=None,
+                 auto_download_file=True,
+                 ):
+        self._raw_details = details
+
+        tvp = self.top_level_attributes
+        self.details = {k: details.get(k, None) for k in tvp}
+        self.details.update(self._flatten_supplementary_attributes())
+
+        for param, value in self.details.iteritems():
+            self.__dict__[param] = value
+
         self._enforce_directories()
 
         self.cached_image_width = cached_image_width \
@@ -37,6 +65,18 @@ class Image:
 
     def __repr__(self):
         return json.dumps(self.details)
+
+    def _flatten_supplementary_attributes(self):
+        flattened = {}
+        a = self.supplementary_attributes_to_flatten
+        for name, (first_lvl_key, second_lvl_key) in a.iteritems():
+            if first_lvl_key in self._raw_details:
+                first_level_dict = self._raw_details[first_lvl_key]
+                if second_lvl_key in first_level_dict:
+                    flattened[name] = first_level_dict[second_lvl_key]
+                    continue
+            flattened[name] = None
+        return flattened
 
     def _enforce_directories(self):
         path = 'images/{}/'.format(self.perma_subdomain)
@@ -62,23 +102,23 @@ class Image:
 
 
 class Grid:
-    def __init__(self, subdomain='slowed', user_id=None):
-        self.subdomain = self._strip_away_url(subdomain)
+    def __init__(self, url='slowed.vsco.co', user_id=None):
+        self.subdomain = self._strip_away_url_elements(url)
         self._enforce_directories()
 
         self.session = self.s = rq.Session()
 
         self.user_id = self._grab_user_id_of_owner() \
             if not user_id else user_id
-
-        self.images = [image for image in self._generate_images(cached_image_width=300)]
+        gen_images_300_px_wide = self._generate_images(cached_image_width=300)
+        self.images = [image for image in gen_images_300_px_wide]
 
     def _enforce_directories(self):
         path = 'meta/{}/'.format(self.subdomain)
         if not osp.isdir(path):
             os.makedirs(path)
 
-    def _strip_away_url(self, url):
+    def _strip_away_url_elements(self, url):
         subdomain_searcher = '(https?:\/\/)?(?P<subdomain>\w+)\.vsco\.co'
         matched = re.search(subdomain_searcher, url)
         if matched:
@@ -152,9 +192,49 @@ class Grid:
         assert 'vs' in self.s.cookies
         return self.s.cookies['vs']
 
+    @property
+    def size(self):
+        return len(self.images)
+
     def grid_page_url(self, page):
         return self.grid_url.replace('1', str(page))
 
+    def grab_attribute_from_all_images(self, attribute):
+        values = {}
+        for image in self.images:
+            attribute_value = image.details.get(attribute, None)
+            if attribute_value is not None:
+                values[image._id] = attribute_value
+        return values
+
+    def histogram_attribute(self,
+                            attribute,
+                            proportional_values=False,
+                            ascending=False
+                            ):
+        histogram = defaultdict(int)
+        attributes = self.grab_attribute_from_all_images(attribute)
+        for v in attributes.values():
+            histogram[v] += 1
+
+        if proportional_values:
+            total = float(len(attributes))
+            histogram = {k: (v / total) for k, v in histogram.iteritems()}
+
+        items = histogram.items()
+        items_sorted = sorted(items, key=lambda t: t[1], reverse=(not ascending))
+        ordered = OrderedDict(items_sorted)
+
+        return ordered
+
+
+class Analyzer:
+    def __init__(self, grid, *args, **kwargs):
+        pass
 
 if '__main__' in __name__:
-    grid = Grid(subdomain='luciomx')
+    grid = Grid(url='luciomx')
+    histo = grid.histogram_attribute('preset')
+    pp(histo, indent=4)
+    for k, v in histo.items():
+        print '{}: {}'.format(k, v)
